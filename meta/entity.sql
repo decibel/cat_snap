7548af5043cfae5f6414f0dcafe5ddb0553fd78d
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-03-26 17:07:23 -0500

    Whitespace

--- meta/entity.sql
+++ meta/entity.sql
@@ -12,12 +12,12 @@ SELECT format(
     relname = 'pg_stat_statements'
     OR (
       relschema='pg_catalog'
-    AND (
-        ( relkind = 'r' AND relname !~ '^pg_(authid|statistic)' )
-        OR ( relkind = 'v' AND (
-              relname = 'pg_stat_user_functions' 
-              OR ( relname ~ '^pg_stat' AND relname !~ '_(user|sys)_' )
-              OR relname !~ '^pg_(group|indexes|shadow|tables|user|views)'
+      AND (
+          ( relkind = 'r' AND relname !~ '^pg_(authid|statistic)' )
+          OR ( relkind = 'v' AND (
+                relname = 'pg_stat_user_functions' 
+                OR ( relname ~ '^pg_stat' AND relname !~ '_(user|sys)_' )
+                OR relname !~ '^pg_(group|indexes|shadow|tables|user|views)'
           ) )
       )
     )
