455c7e4fc53198a8c2b46038a63397406c3f5b92
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-03-26 17:44:42 -0500

    Attempt at adding attributes that didn't work

--- meta/entity.sh
+++ meta/entity.sh
@@ -2,10 +2,22 @@
 
 cat << _EOF_
 CREATE TABLE entity(
-	entity		text		NOT NULL PRIMARY KEY
-	, stat		boolean		NOT NULL
+	entity				text		NOT NULL PRIMARY KEY
+	, is_stat			boolean		NOT NULL
+	, attributes		text[]		NOT NULL
+	, attribute_types	regtype[]	NOT NULL
+	, subtract_keys		text[]		
+	, subtract_counters	text[]		
+	, subtract_fields	text[]		
 );
 
+CREATE VIEW entity_meta AS
+	SELECT *
+			, 
+COMMENT ON COLUMN entity.is_stat IS 'True if this entity is a statistic entity.';
+COMMENT ON COLUMN entity.subtract_keys IS 'Fields used to verify that two stat types that are being subtracted from each other refer to the same entity.';
+COMMENT ON COLUMN entity.subtract_fields IS 'Fields that are counters. These can not be subtracted normally; they require special logic.';
+COMMENT ON COLUMN entity.subtract_fields IS 'Fields to subtract when performing subtraction.';
 _EOF_
 
 psql -qt -v ON_ERROR_STOP=1 -f meta/entity.sql || exit 1
--- meta/entity.sql
+++ meta/entity.sql
@@ -3,11 +3,36 @@ SET client_min_messages = WARNING;
 CREATE EXTENSION IF NOT EXISTS cat_tools;
 
 SELECT format(
-      $$INSERT INTO entity VALUES( %L, %L );$$
-      ,relname::text
-      , relname ~ '^pg_stat'
+      $$INSERT INTO entity VALUES( %L, %L, %L, %L, %L, %L );$$
+      , relname
+      , is_stat
+      , attributes
+      , attribute_types
+      , CASE WHEN is_stat THEN
+          CASE WHEN relname = 'pg_stat_statements' THEN subtract_keys || array['queryid'] ELSE subtract_keys END
+        END
+      , CASE WHEN is_stat THEN subtract_counters END
+      , CASE WHEN is_stat THEN subtract_fields END
     )
-  FROM _cat_tools.pg_class_v r
+  FROM (
+/*
+ * Get all catalog and stat entries, along with details of their attributes and
+ * how to subtract
+ */
+SELECT
+    relname::text
+    , relname ~ '^pg_stat' AS is_stat
+    , array_agg(attname) AS attributes
+    , array_agg(
+        CASE
+          WHEN column_type IN ( 'name', 'anyarray' ) THEN 'text'
+          ELSE column_type
+        END
+      ) AS attribute_types
+    , array_agg( CASE WHEN attname = 'oid' THEN attname::text END ) AS subtract_keys
+    , array_agg( CASE WHEN column_type IN ( 'double precision', 'bigint' ) THEN attname::text END ) AS subtract_counters
+    , array_agg( CASE WHEN column_type IN ( 'timestamp with timezone' ) THEN attname::text END ) AS subtract_fields
+  FROM _cat_tools.column c
   WHERE
     relname = 'pg_stat_statements'
     OR (
@@ -21,8 +46,9 @@ SELECT format(
           ) )
       )
     )
+  GROUP BY relname
+) a
   ORDER BY relname
 ;
-
 
 -- vi: expandtab ts=2 sw=2
