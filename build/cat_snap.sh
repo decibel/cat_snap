242960f5d6057c9241910140f8e38b7c2cc87b76
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-03-29 14:07:39 -0500

    Refactor cat_snap.sh

--- build/cat_snap.sh
+++ build/cat_snap.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo_cat() {
+  echo '-- THIS IS A GENERATED FILE. DO NOT EDIT!'
+  echo "-- Generated from $1"
+  cat $1
+}
+
 cat << _EOF_
 -- THIS IS A GENERATED FILE. DO NOT EDIT!
 CREATE SCHEMA cat_snap;
@@ -8,10 +14,13 @@ CREATE SCHEMA _cat_snap;
 SET search_path = _cat_snap, cat_snap;
 _EOF_
 
+echo_cat generated/entity.dmp
+
 echo '-- THIS IS A GENERATED FILE. DO NOT EDIT!'
-cat generated/entity.dmp
-echo '-- THIS IS A GENERATED FILE. DO NOT EDIT!'
+echo '-- Generated from generated/types.sql'
 echo 'SET search_path = cat_snap, _cat_snap;'
 cat generated/types.sql
-echo '-- THIS IS A GENERATED FILE. DO NOT EDIT!'
-cat build/delta.sql
+
+echo_cat build/delta.sql
+
+# vi: expandtab ts=2 sw=2
