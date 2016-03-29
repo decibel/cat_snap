dee8b8b057d1764bbbdc92e549ec9fbd328ffb72
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-03-29 14:12:15 -0500

    Update tests

--- test/sql/base.sql
+++ test/sql/base.sql
@@ -2,7 +2,30 @@
 
 \i test/pgxntool/setup.sql
 
-SELECT plan(4);
+CREATE TEMP TABLE gather_code_expected(
+  version numeric
+  , expected text
+);
+INSERT INTO gather_code_expected VALUES
+    ( 9.2, 'NULL, NULL, state, NULL, NULL, query, waiting' )
+  , ( 9.3, 'NULL, NULL, state, NULL, NULL, query, waiting' )
+  , ( 9.4, 'NULL, NULL, state, backend_xid, backend_xmin, query, waiting' )
+  , ( 9.5, 'NULL, NULL, state, backend_xid, backend_xmin, query, waiting' )
+  , ( 9.6, 'wait_event_type, wait_event, state, backend_xid, backend_xmin, query, NULL' )
+;
+UPDATE gather_code_expected SET expected =
+  'SELECT datid, datname, pid, usesysid, usename, application_name, client_addr, client_hostname, client_port, backend_start, xact_start, query_start, state_change, '
+    || expected
+    || ' FROM pg_catalog.pg_stat_activity'
+;
+
+SELECT plan(
+  1   -- entities
+  +1  -- types
+  +1  -- delta
+  +1  -- extra_attributes
+  +(SELECT count(*)::int FROM gather_code_expected)
+);
 
 SELECT is(
   (SELECT count(*)::int FROM _cat_snap.entity)
@@ -27,6 +50,14 @@ SELECT is(
   , array[row('waiting','boolean')::_cat_snap.attribute] 
   , 'Verify waiting exists in pg_stat_activity attributes'
 );
+
+SELECT is(
+      cat_snap.gather_code( version, 'pg_stat_activity' )
+      , expected
+      , 'Verify gather code for ' || version || ' pg_stat_activity'
+    )
+  FROM gather_code_expected
+;
 
 \i test/pgxntool/finish.sql
 
